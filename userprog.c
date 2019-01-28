0c8c5f5dfb8ebcb35cfbb545eae8cc6c54005ee6
udit01 <uditjainiitd@gmail.com>
2019-01-28 22:26:13 +0530

    Added a userprog in xv6

---
+++ userprog.c
@@ -0,0 +1,10 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+ 
+int
+main(void)
+{
+  printf(1, "####\n# A user program/function which could be executed in xv6.\n# Developer: Udit Jain\n####\n");
+  exit();
+}
